51ff734e7b4d30298ad37bd125e91218ec522401
Jay Francis <jay.francis@justdevelop.it>
2013-07-17 14:52:46 +0100

    Project file initiates the project by checking if it has been extended and flagging it in the config, along with the extended project path for use in template overrides etc. It will route any URI beginning with /admin to the "BackApp" as a placeholder, otherwise it forwards the request onto "FrontApp", which is determined by checking for an override class based on the project namespace.

--- src/Qubes/Support/Project.php
+++ src/Qubes/Support/Project.php
@@ -2,27 +2,86 @@
 /**
  * @author Jay Francis <jay.francis@justdevelop.it>
  */
+
 namespace Qubes\Support;
 
-use Cubex\Core\Application\Application;
-use Qubes\Support\Applications\Base\BaseApp;
-use Qubes\Support\Applications\Tickets\TicketsApp;
+use Cubex\Core\Interfaces\INamespaceAware;
+use Qubes\Support\Applications\Front\FrontApp;
+use Qubes\Support\Applications\Back\BackApp;
+use Cubex\Foundation\Container;
+use Cubex\Core\Traits\NamespaceAwareTrait;
 
-class Project extends \Cubex\Core\Project\Project
+class Project extends \Cubex\Core\Project\Project implements INamespaceAware
 {
-  protected $_apps = array();
+  use NamespaceAwareTrait;
 
-  protected function _configure()
+  /**
+   * Get the path for the project that includes override templates
+   *
+   * @return string
+   */
+  public function getProjectPath()
   {
-    $this->_addApplication('base', new BaseApp());
-    $this->_addApplication('tickets', new TicketsApp());
+    return __DIR__;
   }
 
-  private function _addApplication($path, Application $application)
+  /**
+   * Initiate the project
+   *
+   * @return $this
+   */
+  public function init()
   {
-    $this->_apps[$path] = $application;
+    $this->_setProjectPathConfig();
+    $this->_setProjectExtendedConfig();
 
     return $this;
+  }
+
+  /**
+   * Set the Project path, and a flag it as extended
+   *
+   * @return $this
+   */
+  private function _setProjectPathConfig()
+  {
+    $config       = Container::config()->get('project');
+    $config->path = $this->getProjectPath();
+
+    return $this;
+  }
+
+  /**
+   * Determine if the project has been extended and set result in config
+   *
+   * @return $this
+   */
+  private function _setProjectExtendedConfig()
+  {
+    $config           = Container::config()->get('project');
+    $config->extended = !($this->getProjectPath() == __DIR__);
+
+    return $this;
+  }
+
+  /**
+   * Get the project title
+   *
+   * @return string
+   */
+  public function getTitle()
+  {
+    return 'Support';
+  }
+
+  /**
+   * Get the project name
+   *
+   * @return string
+   */
+  public function getName()
+  {
+    return 'Support Name';
   }
 
   /**
@@ -32,15 +91,51 @@ class Project extends \Cubex\Core\Project\Project
    */
   public function name()
   {
-    return "Support Tickets";
+    return $this->getName();
   }
 
   /**
-   * @return \Cubex\Core\Application\Application
+   * @return \Cubex\Core\Application\Application|void
+   * @throws \Exception
    */
   public function defaultApplication()
   {
-    echo 1;
-    return new BaseApp();
+    throw new \Exception('Not Implemented');
+  }
+
+  /**
+   * Initial Routing to Application
+   *
+   * @param $path
+   *
+   * @return \Cubex\Core\Application\Application|null|BackApp|FrontApp
+   */
+  public function getByPath($path)
+  {
+    // Back End
+    if(starts_with($path, '/admin'))
+      return new BackApp();
+
+    // Front End
+    return $this->_getFrontApp();
+  }
+
+  /**
+   * Checks for override FrontApp and returns it
+   *
+   * @return FrontApp
+   * @throws \Exception
+   */
+  private function _getFrontApp()
+  {
+    $frontApp = sprintf(
+      '%s\Applications\Front\FrontApp',
+      $this->getNamespace()
+    );
+
+    if(!class_exists($frontApp))
+      throw new \Exception($frontApp . ' Not Found');
+
+    return new $frontApp();
   }
 }
