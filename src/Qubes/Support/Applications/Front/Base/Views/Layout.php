c174aba219190968e08f1d705166f1753499f0f7
Jay Francis <jay.francis@justdevelop.it>
2013-07-18 15:08:50 +0100

    Added the default Layout files and removed unneeded CSS

---
+++ src/Qubes/Support/Applications/Front/Base/Views/Layout.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Default Layout View
+ *
+ * @author Jay Francis <jay.francis@justdevelop.it>
+ */
+namespace Qubes\Support\Applications\Front\Base\Views;
+
+use Cubex\View\TemplatedViewModel;
+
+class Layout extends TemplatedViewModel
+{
+
+}
