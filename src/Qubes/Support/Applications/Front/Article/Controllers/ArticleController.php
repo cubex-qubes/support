e75ad679cbc1b38b4f273e03b14870fd7785d087
Jay Francis <jay.francis@justdevelop.it>
2013-07-18 17:58:56 +0100

    Now checking if the Article exists and rendering a 404 if required

--- src/Qubes/Support/Applications/Front/Article/Controllers/ArticleController.php
+++ src/Qubes/Support/Applications/Front/Article/Controllers/ArticleController.php
@@ -9,6 +9,7 @@ namespace Qubes\Support\Applications\Front\Article\Controllers;
 
 use Qubes\Support\Applications\Front\Base\Controllers\FrontController;
 use Qubes\Support\Applications\Front\Article\Views\ArticleView;
+use Qubes\Support\Components\Content\Article\Article;
 
 class ArticleController extends FrontController
 {
@@ -22,12 +23,20 @@ class ArticleController extends FrontController
    */
   public function renderArticle($id)
   {
+    $article = new Article($id);
+    if(!$article->exists())
+      return $this->renderNotFound();
+
     /** @var ArticleView $view */
     $view = $this->getView('ArticleView');
+    $view->article = $article;
 
     return $view;
   }
 
+  /**
+   * @return array|\Cubex\Routing\IRoute[]
+   */
   public function getRoutes()
   {
     return [
