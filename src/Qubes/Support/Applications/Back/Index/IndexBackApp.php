81c1cedf2aab16f273cee60a23cee3d997355161
Jay Francis <jay.francis@justdevelop.it>
2013-08-05 17:31:48 +0100

    Added IndexBackApp

---
+++ src/Qubes/Support/Applications/Back/Index/IndexBackApp.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Author: oke.ugwu
+ * Date: 02/08/13 08:43
+ */
+
+namespace Qubes\Support\Applications\Back\Index;
+
+use Qubes\Support\Applications\Back\Base\BaseApp;
+
+class IndexBackApp extends BaseApp
+{
+  public function name()
+  {
+    return "Support Center - Index";
+  }
+}
