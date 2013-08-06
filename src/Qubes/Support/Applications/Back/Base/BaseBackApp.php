72ac4631d0fab7aa07b101a9d44bed713bf13134
Oke Ugwu <oke.ugwu@justdevelop.it>
2013-08-06 11:35:30 +0100

    Code Refactor and Rename - Convention shizzle - Changed Controller and App class to abstract classes - Moved default controller to Index app, as it does not belong to BaseApp - Tweaked css by some pixels to align propely with header nav - Changed header nav links so it works with new route logic

--- src/Qubes/Support/Applications/Back/Base/BaseBackApp.php
+++ src/Qubes/Support/Applications/Back/Base/BaseBackApp.php
@@ -7,35 +7,18 @@
 namespace Qubes\Support\Applications\Back\Base;
 
 use Cubex\Core\Application\Application;
-use Qubes\Support\Applications\Back\Base\Controllers\DefaultController;
 use Themed\Sidekick\SidekickTheme;
 
-class BaseBackApp extends Application
+abstract class BaseBackApp extends Application
 {
-  public function __construct()
+  public function init()
   {
     $this->_listen(__NAMESPACE__);
-    $this->setBaseUri('/admin');
   }
 
   public function name()
   {
     return "Support Center";
-  }
-
-  public function defaultController()
-  {
-    return new DefaultController();
-  }
-
-  public function getRoutes()
-  {
-    return [
-      'category/(.*)' => 'CategoryBackController',
-      'article/(.*)'  => 'ArticleBackController',
-      'platform/(.*)' => 'PlatformBackController',
-      'user/(.*)'     => 'UserController'
-    ];
   }
 
   public function getTheme()
