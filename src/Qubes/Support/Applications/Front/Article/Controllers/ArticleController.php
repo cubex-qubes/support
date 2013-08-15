9986b3dcb93d46055909d49dd58de5f0ebd2d170
Jay Francis <jay.francis@justdevelop.it>
2013-08-15 14:58:17 +0100

    Now rendering PDF's for articles based on uri: /article/pdf/{article id}/{platform id}

--- src/Qubes/Support/Applications/Front/Article/Controllers/ArticleController.php
+++ src/Qubes/Support/Applications/Front/Article/Controllers/ArticleController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Qubes\Support\Applications\Front\Article\Controllers;
 
+use dflydev\markdown\MarkdownParser;
 use Qubes\Support\Applications\Front\Base\Controllers\FrontController;
 use Qubes\Support\Applications\Front\Article\Views\ArticleView;
 use Qubes\Support\Components\Content\Article\Mappers\Article;
@@ -30,6 +31,38 @@ class ArticleController extends FrontController
     return $this->setView($view);
   }
 
+  public function renderPdf($id, $platformId)
+  {
+    $article = new Article($id);
+    if(!$article->exists())
+    {
+      return $this->renderNotFound();
+    }
+
+    $markdown = new MarkdownParser();
+
+    $pdf = new \mPDF();
+
+    $pdf->WriteHTML(sprintf('<h1>%s</h1>', $article->title));
+
+    if($article->title)
+    {
+      $pdf->WriteHTML(sprintf('<h2>%s</h2>', $article->subTitle));
+    }
+
+    foreach($article->getArticleSections() as $section)
+    {
+      foreach($section->getBlocks($platformId) as $block)
+      {
+        $pdf->WriteHTML($markdown->transformMarkdown($block->content));
+        $pdf->WriteHTML('<br/>');
+      }
+    }
+
+    $pdf->Output();
+    exit;
+  }
+
   /**
    * @return array|\Cubex\Routing\IRoute[]
    */
@@ -37,6 +70,7 @@ class ArticleController extends FrontController
   {
     return [
       '/article/:id@num:slug' => 'article',
+      '/article/pdf/:id@num/:platformId@num'  => 'pdf',
     ];
   }
 }
